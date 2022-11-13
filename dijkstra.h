756b7c8982db22d2670077ad0c566648b81054c9
Fernando Sousa <96151093+feropiso@users.noreply.github.com>
2022-11-13 15:37:30 -0300

    Create dijkstra.h

---
+++ dijkstra.h
@@ -0,0 +1,26 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+#include "Grafo.h"
+#include <vector>
+using std::vector;
+
+class Dijkstra{
+  
+  public:
+    Dijkstra();
+    Dijkstra(Grafo);
+    ~Dijkstra();
+    void obterArvoreCMC(int);
+    int antecessor(int);
+    double peso(int);
+    void imprimeCaminho(int, int);
+
+  private:
+    Grafo grafo;
+    vector<double*> peso_vertice;
+    vector<int*> antecessores;
+    
+};
+
+
+#endif //DIJKSTRA_H
