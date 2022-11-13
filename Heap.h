b2a1a07b0c248d34785dc13c909bccac95013d83
Fernando Sousa <96151093+feropiso@users.noreply.github.com>
2022-11-13 13:36:11 -0300

    Create Heap.h

---
+++ Heap.h
@@ -0,0 +1,27 @@
+#ifndef HEAP_H
+#define HEAP_H
+#include <vector>
+using std::vector;
+
+class Heap{
+  
+  public:
+    Heap();
+    Heap(vector<double*>, vector<int*>);
+    ~Heap();
+    void refaz(int, int);
+    void controi();
+    int retiraMin();
+    void diminuiChave(int, double);
+    bool vazio();
+
+  private:
+    int n;
+    vector<double*> peso;
+    vector<int*> pos;
+    vector<int*> fp;   
+
+};
+
+
+#endif //HEAP_H
